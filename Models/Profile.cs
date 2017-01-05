d27785d50a05ef65c777055c07d1c1ce52ae5774
batmobiehl <dollarbiehl@gmail.com>
2017-01-04 18:17:46 -0700

    added validations to the model

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -10,21 +10,33 @@ namespace ProfileSearchApp.Models
     public class Profile
     {
         public int ID { get; set; }
+        [Required]
         [Display(Name = "First Name")]
         public string firstName { get; set; }
+        [Required]
         [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Required]
         [Display(Name = "Username")]
         public string userName { get; set; }
+        [Required]
         [Display(Name = "Street")]
         public string street { get; set; }
+        [Required]
         [Display(Name = "City")]
         public string city { get; set; }
+        [Required]
+        [StringLength(2)]
         [Display(Name = "State")]
         public string state { get; set; }
+        [Required]
+        [StringLength(5)]
         [Display(Name = "Zipcode")]
         public string zipcode { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
         public string phoneNumber { get; set; }
+        [Required]
         [Display(Name = "Profile Picture")]
         public string profilePicture { get; set; }
     }
